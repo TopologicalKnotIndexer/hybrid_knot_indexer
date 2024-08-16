81fd32ee53988c83ce432e5daab41704b12f88ea
GGN_2015 <premierbob@qq.com>
2024-08-16 17:16:26 +0800

    fix:path_name_no_space

--- test_all.sh
+++ test_all.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 SCRIPT_PATH=$(readlink -f "$0")
 SCRIPT_FOLDER=$(dirname "$SCRIPT_PATH")
-pushd $SCRIPT_FOLDER > /dev/null
+pushd "$SCRIPT_FOLDER" > /dev/null
 
 bash ./src/HOMFLY-PT-indexer/src/HOMFLY-PT-solver/src/x86_64-sage-minimal/src/bin/portable_sage/test_all.sh
 bash ./python3.sh ./src/main.py --test
