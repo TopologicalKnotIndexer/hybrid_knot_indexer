b1c1a9c3fce576527168d2decca76e7151209325
GGN_2015 <premierbob@qq.com>
2024-08-18 20:46:58 +0800

    fix:folder_rename_for_testcases

--- src/test_all.py
+++ src/test_all.py
@@ -8,9 +8,8 @@ dirnow = os.path.dirname(os.path.abspath(__file__))
 # 不同规则的扭结名称之间遵循着复杂的对应关系
 # 以下函数并不能正确实现这一对应，但对于目前的测试集来说，是够用的
 def link_knot_name(folder_name: str) -> str:
-    assert re.match(r"\d+\_\d+", folder_name) is not None
-    lnum, rnum = [int(v) for v in folder_name.split("_")]
-    return "K%da%d" % (lnum, rnum)
+    assert re.match(r"K\d+(a|n)\d+", folder_name) is not None
+    return folder_name
 
 def get_all_file_in_dir(dirpath: str) -> list: # 获取一个文件夹中的所有文件的绝对路径
     ans = []
