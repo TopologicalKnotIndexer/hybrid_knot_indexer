f7020bb5edd5e324330b3c06dd1a62ab2630aeae
GGN_2015 <premierbob@qq.com>
2024-08-18 23:44:49 +0800

    feat:rename_files

--- src/test_all.py
+++ src/test_all.py
@@ -8,7 +8,7 @@ dirnow = os.path.dirname(os.path.abspath(__file__))
 # 不同规则的扭结名称之间遵循着复杂的对应关系
 # 以下函数并不能正确实现这一对应，但对于目前的测试集来说，是够用的
 def link_knot_name(folder_name: str) -> str:
-    assert re.match(r"K\d+(a|n)\d+", folder_name) is not None
+    assert re.match(r"((m|)K\d+(a|n)\d+(,|))+", folder_name) is not None
     return folder_name
 
 def get_all_file_in_dir(dirpath: str) -> list: # 获取一个文件夹中的所有文件的绝对路径
@@ -36,8 +36,8 @@ def main(): # 进行了测试
     for file in list_all_test_file():
         knotname_ans = link_knot_name(get_knot_name_from_path(file))
         knotname_got = che_file_to_knot_name.che_file_to_knot_name(file)
-        suc_test     = (knotname_ans in knotname_got or ("m" + knotname_ans) in knotname_got) # 成功标志
-        print("\033[1;34mINFO\033[0m: testing for knot type (\033[1;33m%-5s\033[0m) ... " % knotname_ans, end="") # 输出测试信息
+        suc_test     = (knotname_ans in knotname_got) # 成功标志
+        print("\033[1;34mINFO\033[0m: testing for knot type (\033[1;33m%-6s\033[0m) ... " % knotname_ans, end="") # 输出测试信息
         print(
             "\033[1;32m[    PASSED]\033[0m" if suc_test else # 输出成功标志
             "\033[1;31m[NOT PASSED]\033[0m"
